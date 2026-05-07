802ff49be8c42550fc1c6d753e936d83994358fb
Elhussein <elhusseinsalah565@gmail.com>
2026-05-07 14:16:49 +0300

    feat: remove helmet middleware to simplify security configuration

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
-
-  app.use(
-    helmet({
-      contentSecurityPolicy: false,
-      crossOriginEmbedderPolicy: false,
-      crossOriginOpenerPolicy: false,
-      crossOriginResourcePolicy: false,
-      hsts: false,
-    }),
-  );
 
   const config = new DocumentBuilder()
     .setTitle('Reddex')
